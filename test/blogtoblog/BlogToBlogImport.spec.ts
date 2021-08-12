2bda581c7de5534e2fdb9717536a3d797bf423e6
Katie Sexton <ksexton@adobe.com>
2021-08-12 13:10:56 -0600

    feat: buildmetadata test

--- test/blogtoblog/BlogToBlogImport.spec.ts
+++ test/blogtoblog/BlogToBlogImport.spec.ts
@@ -16,6 +16,7 @@ import { strictEqual } from 'assert';
 import { describe, it } from "mocha";
 
 import { JSDOM } from 'jsdom';
+import { createStrictEquality } from 'typescript';
 
 const getImporter = (): BlogToBlogImporter => {
   return new BlogToBlogImporter({
@@ -38,5 +39,58 @@ describe('BlogToBlogImporter#convertBlocksToTables tests', () => {
     test(
       `<main>${div}${div}${div}<div><div class="block-1"><div>header cell</div><div>first row one cell</div></div></main>`,
       `<main>${div}${div}${div}<div><table><tr><th>block-1</th></tr></table></div></main>`);
+  });
+});
+describe('BlogToBlogImporter#buildMetadataTable tests', () => {
+  const test = (input: string, expected: string) => {
+    const { document } = (new JSDOM(input)).window;
+    getImporter().buildMetadataTable(document, document);
+    strictEqual(document.body.innerHTML, expected);
+  };
+
+  const div = `<div></div>`;
+
+  it('build metadata table', () => {
+    test(
+      `<main>
+        ${div}
+        ${div}
+        <div>
+          <p>By Katie Sexton</p>
+          <p>Posted on 09-09-2019</p>
+        </div>
+        ${div}
+        <div>
+          <p>Topics: Alpha, Beta, Gamma</p>
+          <p>Products: Delta, Echo, Foxtrot</p>
+        </div>
+      </main>`,
+      `<main>
+        ${div}
+        ${div}
+        ${div}
+        <div>
+          <table>
+            <tr>
+              <th>metadata</th>
+            </tr>
+            <tr>
+              <td>Author</td>
+              <td>Katie Sexton</td>
+            </tr>
+            <tr>
+              <td>Publication Date</td>
+              <td>09-09-2019</td>
+            </tr>
+            <tr>
+              <td>Category</td>
+              <td>Alpha</td>
+            </tr>
+            <tr>
+              <td>Topics</td>
+              <td>Beta, Gamma, Delta, Echo, Foxtrot</td>
+            </tr>
+          </table>
+        </div>`);
   });
 });
