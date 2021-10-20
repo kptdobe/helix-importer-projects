26dc28de2fa80ae2d53e9897f9845e22cd6f0602
Katie Sexton <ksexton@adobe.com>
2021-10-20 11:35:38 -0600

    feat: author, banner importer

---
+++ src/blogtobusiness-authors/BlogToBusinessAuthorImporter.ts
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-disable no-console */
+
+import { PageImporter, PageImporterResource, DOMUtils, WPUtils } from '@adobe/helix-importer';
+
+import fetch from 'node-fetch';
+import path from 'path';
+import { Response } from 'node-fetch';
+import { Document } from 'jsdom';
+
+import Blocks from '../utils/Blocks';
+import DOM from '../utils/DOM';
+
+export default class BlogToBlogImporter extends PageImporter {
+  async fetch(url): Promise<Response> {
+    return fetch(url);
+  }
+
+  updateHeadings(main: Element, document: Document): void {
+    const heading = main.querySelector('h1');
+    if (!heading) {
+      const nextHeading = main.querySelector('h2, h3, h4, h5, h6');
+      const newHeading = document.createElement('h1');
+      newHeading.textContent = nextHeading.textContent;
+      nextHeading.replaceWith(newHeading);
+    }
+  }
+
+  rewriteImgSrc(main: Element): void {
+    main.querySelectorAll('img').forEach((img) => {
+      const { src } = img;
+      if (src && src.indexOf('?') !== -1) {
+        img.src = src.split('?')[0];
+      }
+    });
+  }
+
+  rewriteLinks(main: Element): void {
+    main.querySelectorAll('a').forEach((a) => {
+      const { href, innerHTML } = a;
+      // TODO: use outer cdn URL
+      if (href.startsWith('https://blog.adobe.com/')) {
+        a.href = href.replace('https://blog.adobe.com/', 'https://main--business-website--adobe.hlx3.page/');
+        a.innerHTML = innerHTML.replace('https://blog.adobe.com/', 'https://main--business-website--adobe.hlx3.page/');
+      }
+    });
+  }
+
+  async process(document: Document, url: string, entryParams?: any): Promise<PageImporterResource[]> {
+    DOMUtils.remove(document, [
+      'header',
+      'footer',
+    ]);
+
+    const main = document.querySelector('main');
+
+    const u = new URL(url);
+    const p = path.parse(u.pathname);
+    const name = p.name;
+
+    const pir = new PageImporterResource(
+      name,
+      `authors`,
+      main,
+      null,
+      {},
+    );
+
+    return [pir];
+  }
+}
---
+++ src/blogtobusiness-authors/import.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import BlogToBlogAuthorImporter from './BlogToBusinessAuthorImporter';
+
+import { FSHandler, Utils } from '@adobe/helix-importer';
+import { BlobHandler } from '@adobe/helix-documents-support';
+
+import { config } from 'dotenv';
+import fetch from 'node-fetch';
+
+// tslint:disable: no-console
+
+config();
+
+const [argMin, argMax] = process.argv.slice(2);
+
+function sectionData(data, min, max) {
+  min = min ? Number(min) : 0;
+  max = max ? Number(max) : data.length;
+  const section = data.slice(min, max);
+  return section;
+}
+
+async function getAuthors() {
+  const req = await fetch('https://main--business-website--adobe.hlx3.page/drafts/fkakatie/importer-authors.json');
+  if (req.ok) {
+    const { data } = await req.json();
+    return data;
+  }
+  return [];
+}
+
+async function main() {
+  const handler = new FSHandler('output/blogtoblog-authors', console);
+  // tslint:disable-next-line: no-empty
+  const noop = () => {};
+  const blob = new BlobHandler({
+    skipSchedule: true,
+    azureBlobSAS: process.env.AZURE_BLOB_SAS,
+    azureBlobURI: process.env.AZURE_BLOB_URI,
+    log: {
+      debug: noop,
+      info: noop,
+      warn: noop,
+      error: () => console.error(...arguments),
+    },
+  });
+
+  const allAuthors = await getAuthors();
+  const authors = sectionData(allAuthors, argMin, argMax);
+
+  const importer = new BlogToBlogAuthorImporter({
+    storageHandler: handler,
+    blobHandler: blob,
+    cache: '.cache/blogtoblog-authors',
+    skipAssetsUpload: true,
+    // skipDocxConversion: false,
+    // skipMDFileCreation: true,
+  });
+
+  // let output = `source;file;lang;author;date;category;topics;tags;banners;\n`;
+  await Utils.asyncForEach(authors, async (e) => {
+    try {
+
+      console.log(e.author);
+      const cleanName = e.author.toLowerCase().split(' ').join('-');
+      const url = `https://blog.adobe.com/en/authors/${cleanName}`;
+      console.log(url);
+
+      const resources = await importer.import(url);
+
+      // resources.forEach((entry) => {
+      //   console.log(`${entry.source} -> ${entry.file}`);
+      //   output += `${entry.source};${entry.file};${entry.extra.lang};${entry.extra.author};${entry.extra.date};${entry.extra.category};${entry.extra.topics};${entry.extra.tags};${entry.extra.banners}\n`;
+      // });
+      // await handler.put('banners-import_output.csv', output);
+    } catch(error) {
+      console.error(`Could not import ${e.author}`);
+    }
+  });
+  console.log('Done');
+}
+
+main();
