0e78affd58107a30641074839a5ab84883204f9a
Katie Sexton <ksexton@adobe.com>
2021-11-01 16:19:28 -0600

    feat: write author import files

--- src/blogtoblog/cli/authors.ts
+++ src/blogtoblog/cli/authors.ts
@@ -6,21 +6,21 @@ import siteconfig from '../config';
 import { config } from 'dotenv';
 config();
 
-const PROMOTIONS_PATH = '/promotions';
+const AUTHORS_PATH = '/authors';
 
 async function main(lang) {
   const entries = await fg('**/*.{docx,md}', {
-    cwd: `${process.env.BLOGTOBLOG_SRC_FOLDER}/${lang}${PROMOTIONS_PATH}`,
+    cwd: `${process.env.BLOGTOBLOG_SRC_FOLDER}/${lang}${AUTHORS_PATH}`,
   });
 
   const rows = [];
   rows.push([
     'currentPath',
     'newPath',
-    'selector',
+    'name',
   ]);
   entries.forEach((e) => {
-    const currentPath = `${PROMOTIONS_PATH}/${e.split('.')[0]}`;
+    const currentPath = `${AUTHORS_PATH}/${e.split('.')[0]}`;
 
     const [filename, dirname] = currentPath.split('/').reverse();
     const [basename] = filename.split('.');
@@ -28,17 +28,17 @@ async function main(lang) {
     const selectorBasename = (basename || '').toLowerCase().replace(/[^a-z0-9]/g, '');
     const selectorDirname = (dirname || '').toLowerCase().replace(/[^a-z0-9]/g, '');
 
-    const selector = `embed embed-internal embed-internal-${selectorBasename} embed-internal-${selectorDirname}`;
+    const name = e.split('.')[0].toLowerCase();
 
-    const newPath = currentPath.toLowerCase();
+    const newPath = currentPath.replace(/ /g, '-').toLowerCase();
 
     // tslint:disable-next-line: no-console
-    // console.log(currentPath, newPath, selector);
+    // console.log(currentPath, newPath, name);
 
     rows.push([
       currentPath,
       newPath,
-      selector,
+      name,
     ]);
   });
 
@@ -47,7 +47,7 @@ async function main(lang) {
   sheet.addRows(rows);
   const dir = `output/blogtoblog/${lang}/drafts/import/`;
   await fs.ensureDir(dir);
-  await workbook.xlsx.writeFile(`${dir}/promotions.xlsx`);
+  await workbook.xlsx.writeFile(`${dir}/authors.xlsx`);
 }
 
 siteconfig.LOCALES.forEach(l => main(l));
