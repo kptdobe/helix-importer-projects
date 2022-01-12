25103094063da5770ff79ca04c0f94b7dd31cc70
Alexandre Capt <acapt@adobe.com>
2022-01-12 09:48:26 +0100

    feat: sitemap loader

---
+++ src/utils/Loader.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export default class Loader {
+  static async loadSitemap(sitemapURL) {
+    const resp = await fetch(sitemapURL);
+    const xml = await resp.text();
+    const sitemap = new DOMParser().parseFromString(xml, 'text/xml');
+    const subSitemaps = [...sitemap.querySelectorAll('sitemap loc')];
+    let paths = [];
+    const promises = subSitemaps.map(loc => new Promise((resolve) => {
+      const subSitemapURL = new URL(loc.textContent);
+      Loader.loadSitemap(subSitemapURL.pathname).then((result) => {
+        paths = paths.concat(result);
+        resolve(true);
+      });
+    }));
+
+    await Promise.all(promises);
+
+    const urlLocs = sitemap.querySelectorAll('url loc');
+    urlLocs.forEach((loc) => {
+      const locURL = new URL(loc.textContent);
+      paths.push(locURL.pathname);
+    });
+
+    return paths;
+  }
+}
